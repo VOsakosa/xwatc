24b3608e1453f4a8ee1d663775603b2e1ab08265
Leo Ischebeck <leoischebeck@gmail.com>
2020-10-12 19:03:34 +0200

    Mia Start

---
+++ xwatc/Mias_Teilgeschichte.py
@@ -0,0 +1,8 @@
+from time import sleep
+import xwatc_Hauptgeschichte as xwatc
+from xwatc.system import Mänx, minput, Gefährte, ja_nein, mint, Spielende
+from xwatc import jaspersteilgeschichte
+import random
+
+mint("Hurra! Nun bist du in Mias Teilgeschichte!!! Freu dich und freu dich ein weiteres mal!")
+mint("Juheirassa!")
