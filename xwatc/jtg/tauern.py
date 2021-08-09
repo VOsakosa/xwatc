660c95fb2c30cd81abe1007dfdd3879dc496c961
Jasper Ischebeck <jasper@fjink>
2021-08-09 17:13:38 +0200

    Tauern Gold

--- xwatc/jtg/tauern.py
+++ xwatc/jtg/tauern.py
@@ -3,9 +3,23 @@ Tauern, das Land der aufrechten Kühe.
 Created on 15.10.2020
 """
 from xwatc import weg
+from xwatc.dorf import NSC
 __author__ = "jasper"
-from xwatc.system import Mänx
+from xwatc.system import Mänx, register
 from xwatc.weg import gebiet, WegAdapter, Wegkreuzung, Wegtyp
+
+ZOLL_PREIS = 10
+
+KRIEGER_INVENTAR = {
+    "Axt": 1,
+    "Lederrüstung": 1,
+    "Lederhose": 1,
+    "Lendenschurz": 1,
+    "Leibchen": 1,
+    "Armband": 1,
+    "Sandalen": 1,
+    "Gold": 10
+}
 
 
 def land_der_kühe(mänx: Mänx):
@@ -50,10 +64,41 @@ def erzeuge_tauern(__) -> WegAdapter:
         "Du kommst an einen steilen Berg, der in den Fluss "
         "ragt. Ohne Kletterkünste ist hier kein Vorbeikommen.")
 
+    zoll = weg.WegSperre(None, None, zoll_fn, zoll_fn)
+    vorbrück.verbinde(zoll, "o", ziel="Über die Brücke")
+    
+    hinterbrück = Wegkreuzung("hinterbrück", immer_fragen=True, w=zoll)
+    hinterbrück.add_beschreibung("Vor dir liegt nun Tauern.", "w")
+    hinterbrück.add_beschreibung("Du kommst an eine Zollbrücke.", "o")
+
     return ein_adap
+
+
+def zoll_fn(mänx: Mänx):
+    wächter = mänx.welt.obj("jtg:tau:wächter")
+    if wächter.tot:
+        mänx.ausgabe.malp("Das Zollhaus ist leer.")
+        return True
+    kosten = (1 + len(mänx.gefährten)) * ZOLL_PREIS
+    wächter.sprich(f"Das macht dann {kosten} Gold.")
+    if mänx.ja_nein("Zahlst du das Geld?"):
+        mänx.erhalte("Gold", -kosten)
+        return True
+    else:
+        return False
+
+
+@register("jtg:tau:wächter")
+class Zollwärter(NSC):
+    """Der Zollwärter bewacht die Brücke."""
+
+    def __init__(self):
+        super().__init__("Federico", "Pestalozzi",
+                         startinventar=KRIEGER_INVENTAR)
+        self.inventar["Uniform"] += 1
+        self.rasse = "Mumin"
 
 
 if __name__ == '__main__':
     import xwatc.anzeige
     xwatc.anzeige.main(land_der_kühe)
-    
