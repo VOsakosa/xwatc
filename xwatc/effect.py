c870e17b75382a88042af618cc67731e07e96660
Jasper Ischebeck <jasper@fjink>
2023-03-31 11:46:41 +0200

    Converter, um str statt Funktionen geben zu können

--- xwatc/effect.py
+++ xwatc/effect.py
@@ -4,17 +4,39 @@ müssen. Das ist dann leichter zu speichern.
 
 Created on 31.03.2023
 """
-from attrs import define
-from xwatc.system import MänxPrädikat, Fortsetzung, MänxFkt, Mänx
+from attrs import define, field
+from collections.abc import Sequence
+from xwatc.system import MänxPrädikat, Fortsetzung, MänxFkt, Mänx, malp
 __author__ = "jasper"
+
+
+def _to_geschichte(text: str | Sequence[str] | MänxFkt[Fortsetzung | None]
+                   ) -> MänxFkt[Fortsetzung | None]:
+    match text:
+        case str(txt):
+            return TextGeschichte([txt])
+        case Sequence():
+            return TextGeschichte(text)
+        case _:
+            return text
+
+
+@define
+class TextGeschichte:
+    texte: Sequence[str]
+
+    def __call__(self, _mänx: Mänx) -> None:
+        for text in self.texte:
+            malp(text)
 
 
 @define
 class NurWenn:
     """Diese Geschichte wird nur durchgeführt, wenn eine Variable gesetzt ist."""
     prädikat: MänxPrädikat
-    geschichte: MänxFkt[Fortsetzung | None]
-    sonst: MänxFkt[Fortsetzung | None] | None = None
+    geschichte: MänxFkt[Fortsetzung | None] = field(converter=_to_geschichte)
+    sonst: MänxFkt[Fortsetzung | None] | None = field(
+        converter=_to_geschichte, default=None)
 
     def __call__(self, mänx: Mänx) -> Fortsetzung | None:
         if self.prädikat(mänx):
@@ -42,7 +64,3 @@ class Einmalig:
             return False
         mänx.welt.setze(self.id_)
         return True
-
-
-def einmalig(id_: str, geschichte: MänxFkt[Fortsetzung | None]) -> NurWenn:
-    return NurWenn(Einmalig(id_), geschichte)
