2eb7b02dea39b92d54ec4138590a16d094ce4523
Jasper Ischebeck <jasper@fjink>
2023-03-31 10:44:21 +0200

    Räume Norden-Alt auf

--- xwatc/lg/norden/norden.py
+++ xwatc/lg/norden/norden.py
@@ -6,47 +6,43 @@ from xwatc.lg import mitte
 
 norden = Eintritt(("lg:norden", "süden"))
 
+
 @gebiet("lg:norden")
 def erzeuge_norden(mänx: Mänx, gb: Gebiet) -> None:
-    gäfdah = mänx.welt.get_or_else("Gäfdah", gäfdah_module.erzeuge_Gäfdah, mänx)
-    gb.setze_punkt((1,1), gäfdah.draußen)
-    gäfdah.draußen.verbinde(Gebietsende(None, gb, "süden", mitte.MITTE, "norden"), "s")
+    gäfdah = mänx.welt.get_or_else(
+        "Gäfdah", gäfdah_module.erzeuge_Gäfdah, mänx)
+    gb.setze_punkt((1, 1), gäfdah.draußen)
+    gäfdah.draußen.verbinde(Gebietsende(
+        None, gb, "süden", mitte.MITTE, "norden"), "s")
     gäfdah.draußen.add_beschreibung([
         "Du wanderst 9 Tage lang gen Norden, bis du zu einem kleinen Fischerdorf "
         "kommst."], nur="s")
-    
+
 
 def norden_alt(mänx: Mänx) -> Fortsetzung | None:
     # TODO Den Bettler wieder einbauen
     malp("Du gehst weiter und triffst auf einen Bettler.")
     if mänx.hat_item("Gold"):
-        malp("Du kannst ihm ein Stück Geld geben oder "
-             "weitergehen.")
+        malp("Du kannst ihm ein Stück Geld geben oder weitergehen.")
         if ja_nein(mänx, "Gibst du ihm Geld?"):
             mänx.inventar["Gold"] -= 1
             malp("Der Bettler blickt dich dankend an.")
-            malp("Du erhältst einen Stein der Bettlerfreundschaft.")
-            mänx.inventar["Stein der Bettlerfreundschaft"] += 1
+            mänx.erhalte("Stein der Bettlerfreundschaft")
 
     else:
         malp("Du hast leider kein Geld.")
 
     entscheidung = minput(mänx, "Du wanderst durch das kleine Dorf. Einige Gassenjungen "
-                          "folgen dir. Bleibst du im Dorf oder gehst du weiter? "
-                          "bleiben/w", ["bleiben", "w"])
+                          "folgen dir. Bleibst du im Dorf oder gehst du weiter?", ["bleiben", "w"])
     if entscheidung == "w":
-        weg = minput(mänx, "Gehst du in die R"
-                     "ichtung aus der du gekom"
-                     "men bist, in eine völlig"
-                     " andere oder weiter in Ri"
-                     "chtung Norden? z/a/w"
-                     "zurück/anders/weiter", ["z", "w", "a"])
+        weg = minput(mänx, "Gehst du in die Richtung aus der du gekommen bist, in eine völlig"
+                     " andere oder weiter in Richtung Norden?", ["z", "w", "a"])
         if weg == "z":
             return Eintritt((mitte.MITTE, "norden"))
 
         if weg == "a":
             k = minput(
-                mänx, "Gehst du in Richtung Westen oder in Richtung Osten? (w/o", ["w", "o"])
+                mänx, "Gehst du in Richtung Westen oder in Richtung Osten?", ["w", "o"])
             if k == "w":
                 malp("Hallo")
 
