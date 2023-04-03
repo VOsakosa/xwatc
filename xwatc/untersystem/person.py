500550da5b664d08f35c60a2da7a4896afe41199
Jasper Ischebeck <jasper@fjink>
2023-04-03 23:02:18 +0200

    Verschiebe Rasse in ein neues Modul

---
+++ xwatc/untersystem/person.py
@@ -0,0 +1,15 @@
+"""
+Verschiedene Eigenschaften vom Mänxen und anderen Personen.
+Created on 03.04.2023
+"""
+__author__ = "Jasper Ischebeck"
+
+from enum import Enum
+
+
+class Rasse(Enum):
+    """Eine humanoide Rasse."""
+    Mensch = 1
+    Munin = 2
+    Lavaschnecke = 11
+    Skelett = 12
